d123b6774da88ac4166cd64049409832a94fb5bb
sachinarora99 <79194178+sachinarora99@users.noreply.github.com>
2025-07-17 20:41:16 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-  mongoose.connect(process.env.MONGO_URI);
+ await mongoose.connect(process.env.MONGO_URI);
 
-    }
+    
     console.log('MongoDB connected');
   } catch (err) {
     console.error('DB connection failed:', err.message);
